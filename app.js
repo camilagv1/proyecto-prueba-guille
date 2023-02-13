e6cb0dcdf3d2761d20325748e5b231c47f931eb8
From SENA <cuentaparavb@outlook.com>
2023-02-13 17:45:03 -0500

    clase guille servidor

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+//app.use(express.static('public'))
+
+app.use("/resources", express.static("public"));
+app.use("/resources", express.static(__dirname + "/public"));// creamos la carpeta resources
+
+//4- Estableciendo el motor de plantillas
+app.set('view engine','ejs');
+
+//Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
+app.use(express.urlencoded({extended:false}));
+//además le decimos a express que vamos a usar json
+app.use(express.json());
+
+
+ //Establecemos las rutas para las VISTAS usando un archivo aparte (router.js) y la clase Router()
+// app.use('/', require('./router'));
+ app.get('/',(req, res)=>{
+    res.render('index')
+ })
+
+app.listen(4000, ()=>{
+    console.log('SERVER corriendo en http://localhost:4000');
+});
